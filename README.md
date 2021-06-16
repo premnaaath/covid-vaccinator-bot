e48bb42c0e050bfe109177553147275bbb5b9da2
Premnaath <premnaathvaradharajan@gmail.com>
2021-06-16 14:56:51 +0530

    Add files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # covid-bot
+
 A telegram bot that helps you find a COVID vaccine slot.
---
+++ main.py
@@ -0,0 +1,46 @@
+import constants as key
+from get_data import get_data, is_valid_pincode
+import telebot
+
+API_KEY = key.API_KEY
+bot = telebot.TeleBot(API_KEY)
+
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    msg = '''
+Hello there!
+I'm a bot designed to help you find a vaccine slot in your locality...
+
+Please enter your area Pincode.'''
+    bot.send_message(message.chat.id, msg)
+
+
+def temp(message):
+    return True
+
+
+@bot.message_handler(func=temp)
+def send_data(message):
+    msg = get_data(message.text)
+    if len(msg) == 0:
+        temp = ''' 
+Sorry, there are no vaccines available near your locality for today!
+Please try again after sometime...
+        '''
+        bot.send_message(message.chat.id, temp)
+    elif is_valid_pincode(message.text):
+        for i in msg:
+            bot.send_message(message.chat.id, i)
+        text = '''
+For Registration, please visit
+https://selfregistration.cowin.gov.in/
+
+Have a nice day! :)
+        '''
+        bot.send_message(message.chat.id, text=text)
+    else:
+        bot.send_message(message.chat.id, msg[0])
+
+
+bot.polling()
