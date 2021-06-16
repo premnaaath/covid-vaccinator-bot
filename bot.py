0cc3827fd21beace3e21f2f1f8bcde5c80bd595f
Premnaath <premnaathvaradharajan@gmail.com>
2021-06-16 20:22:36 +0530

    Add Flask Functionalities

--- bot.py
+++ bot.py
@@ -1,15 +1,18 @@
+import os
 import constants as key
 from get_data import get_data, is_valid_pincode
+from flask import Flask, request
 import telebot
 
-API_KEY = key.API_KEY
-bot = telebot.TeleBot(API_KEY)
+TOKEN = key.TOKEN
+bot = telebot.TeleBot(TOKEN)
+app = Flask(__name__)
 
-greet_msg = '''
+greet_msg = """
 Hello there!
 I'm a bot designed to help you find a vaccine slot in your locality...
 
-Please enter your area Pincode.'''
+Please enter your area Pincode."""
 
 
 @bot.message_handler(commands=["start"])
@@ -24,26 +27,42 @@ def temp(message):
 @bot.message_handler(func=temp)
 def send_data(message):
     msg = get_data(message.text)
-    if message.text in ['hi', 'hello']:
+    if message.text in ["hi", "hello"]:
         bot.send_message(message.chat.id, greet_msg)
     elif len(msg) == 0:
-        temp = ''' 
+        temp = """ 
 Sorry, there are no vaccines available near your locality for today!
 Please try again after sometime...
-        '''
+        """
         bot.send_message(message.chat.id, temp)
     elif is_valid_pincode(message.text):
         for i in msg:
             bot.send_message(message.chat.id, i)
-        text = '''
+        text = """
 For Registration, please visit
 https://selfregistration.cowin.gov.in/
 
 Have a nice day! :)
-        '''
+        """
         bot.send_message(message.chat.id, text=text)
     else:
         bot.send_message(message.chat.id, msg[0])
 
 
-bot.polling()
+@app.route("/" + TOKEN, methods=["POST"])
+def getMessage():
+    json_string = request.get_data().decode("utf-8")
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
+
+
+@app.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url="https://covid-vaccinator-bot.herokuapp.com/" + TOKEN)
+    return "!", 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
