370d56927af4c482344bf8187e7dc02095afa968
Premnaath <premnaathvaradharajan@gmail.com>
2021-06-17 08:22:38 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,50 +1,48 @@
-import os
 import time
 import telebot
-from apscheduler.schedulers.background import BackgroundScheduler
+import constants as keys
 from get_data import get_data, is_valid_pincode
+from apscheduler.schedulers.background import BackgroundScheduler
 
+API_KEY = keys.API_KEY
+bot = telebot.TeleBot(API_KEY)
 scheduler = BackgroundScheduler()
 scheduler.start()
-TOKEN = "1871608505:AAHTk1het3w4dtxjPIEsz0N6A92aJ6eQBWQ"
-bot = telebot.TeleBot(TOKEN)
-
-
-greet_msg = """
-Hello there!
-I'm a bot designed to help you find a vaccine slot in your locality...
-
-Please enter your area Pincode."""
-
-temp = """ 
-Sorry, there are no vaccines available near your locality for today!
-Please try again after sometime...
-        """
-
-text = """
-For Registration, please visit
-https://selfregistration.cowin.gov.in/
-
-Have a nice day! :)
-        """
 
 
 @bot.message_handler(commands=["start"])
 def start(message):
-    bot.send_message(message.chat.id, greet_msg)
+    msg = '''
+Hello there!
+I'm a bot designed to help you find a vaccine slot in your locality...
+
+Please enter your area Pincode.'''
+    bot.send_message(message.chat.id, msg)
 
 
-@bot.message_handler(func=lambda msg: msg.text is not None and "@" not in msg.text)
+def temp(message):
+    return True
+
+
+@bot.message_handler(func=lambda *_: True)
 def send_data(message):
     msg = get_data(message.text)
-    if message.text in ["hi", "hello"]:
-        bot.send_message(message.chat.id, greet_msg)
-    elif len(msg) == 0:
+    if len(msg) == 0:
+        temp = ''' 
+Sorry, there are no vaccines available near your locality for today!
+Please try again after sometime...
+        '''
         bot.send_message(message.chat.id, temp)
     elif is_valid_pincode(message.text):
         for i in msg:
             bot.send_message(message.chat.id, i)
-        bot.send_message(message.chat.id, text)
+        text = '''
+For Registration, please visit
+https://selfregistration.cowin.gov.in/
+
+Have a nice day! :)
+        '''
+        bot.send_message(message.chat.id, text=text)
     else:
         bot.send_message(message.chat.id, msg[0])
 
@@ -52,5 +50,7 @@ def send_data(message):
 while True:
     try:
         bot.polling(none_stop=True)
+        # ConnectionError and ReadTimeout because of possible timout of the requests library
+        # maybe there are others, therefore Exception
     except Exception:
         time.sleep(15)
